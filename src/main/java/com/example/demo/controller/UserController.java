d34e51d609226e05c7df54c2bcc05182f7f9d6ed
eugeniagiuliodori <eugegiuliodori@gmail.com>
2021-07-05 19:10:20 -0300

    changes in update: revoque token before generate new token rewards always a correct generated new token. And other fews changes in update user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -135,7 +135,7 @@ public class UserController {
                     if ((bodyUser != null && !authenticatedUser.equals(bodyUser))||
                     (bodyPassUser != null && !authuser.getPassword().equals(bodyPassUser)) ||
                             (changesRoles)){
-                        String new_token_refreshToken = revoquesToken(usermod.getName(), tokenValue, auth.getIdClient(), auth.getPassClient());
+                        String new_token_refreshToken = revoquesToken(usermod.getName(),user.getPassword(),user.getPassword() != null ,tokenValue, auth.getIdClient(), auth.getPassClient());
                         if(new_token_refreshToken.contains("error")){
                             new_token_refreshToken="{\"error\":\""+new_token_refreshToken+"\"}";
                         }
@@ -295,30 +295,42 @@ public class UserController {
     }
 
 
-    private String revoquesToken(String userName, String tokenValue, String idClient,String passClient) {
-            String credentials = idClient + ":" + passClient;
+    private String revoquesToken(String userName, String userPass, boolean passInRequest, String tokenValue, String idClient,String passClient) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + tokenValue);
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
+        HttpEntity formEntity = new HttpEntity<MultiValueMap<String, String>>(requestBody, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String access_token_url = "http://localhost:8040/user/logout";
+        restTemplate.exchange(access_token_url, HttpMethod.POST, formEntity, String.class);
+
+
+
+
+
+
+
+
+        String credentials = idClient + ":" + passClient;
             String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.add("Authorization", "Basic " + encodedCredentials);
-            MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
-            requestBody.add("Content-Type", "application/x-www-form-urlencoded");
-            requestBody.add("username", userName);
-            requestBody.add("password", userService.getAuthenticatedPassUser());
-            requestBody.add("grant_type", "password");
-            HttpEntity formEntity = new HttpEntity<MultiValueMap<String, String>>(requestBody, headers);
-            HttpEntity<String> request = new HttpEntity<String>(headers);
-            RestTemplate restTemplate = new RestTemplate();
-            String access_token_url = "http://localhost:8040/oauth/token";
-            ResponseEntity<String> response = restTemplate.exchange(access_token_url, HttpMethod.POST, formEntity, String.class);
             headers = new HttpHeaders();
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-            headers.add("Authorization", "Bearer " + tokenValue);
+            headers.add("Authorization", "Basic " + encodedCredentials);
             requestBody = new LinkedMultiValueMap<String, String>();
+            requestBody.add("Content-Type", "application/x-www-form-urlencoded");
+            requestBody.add("username", userName);
+            if(!passInRequest){
+                userPass=userService.getAuthenticatedPassUser();
+            }
+            requestBody.add("password", userPass);
+            requestBody.add("grant_type", "password");
             formEntity = new HttpEntity<MultiValueMap<String, String>>(requestBody, headers);
+            HttpEntity<String> request = new HttpEntity<String>(headers);
             restTemplate = new RestTemplate();
-            access_token_url = "http://localhost:8040/user/logout";
-            restTemplate.exchange(access_token_url, HttpMethod.POST, formEntity, String.class);
+            access_token_url = "http://localhost:8040/oauth/token";
+            ResponseEntity<String> response = restTemplate.exchange(access_token_url, HttpMethod.POST, formEntity, String.class);
             return response.getBody();
     }
 
